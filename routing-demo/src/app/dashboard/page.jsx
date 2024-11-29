83094b7ba4bd8525183da0971db0679d7882727b
ashokneupane100 <ashokabrother@gmail.com>
2024-11-29 18:41:40 +0545

    File Colocation system in next.js

---
+++ routing-demo/src/app/dashboard/page.jsx
@@ -0,0 +1,15 @@
+function BarChart() {
+  return <h1>This is the Bar Chart</h1>;
+}
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>
+        This is the dashboard page here right now. haahaa haa . Ajja raatbhari
+        tyahi herera baschha. kera herera baschha. tyatro time spent bhaeko chha
+        without any goodness at the moment.
+      </h1>
+    </div>
+  );
+}
