54c78d20a831f566628d641adc6a097aaf3c38ce
ashokneupane100 <ashokabrother@gmail.com>
2024-11-29 17:14:12 +0545

    Page not found system in nextJS

---
+++ routing-demo/src/app/not-found.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Your Requested Page Not Found. please type the correct slug please.</h2>
+      <p>Could not find the requested resource.</p>
+    </div>
+  );
+}
---
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/not-found.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Review Not Found </h2>
+    </div>
+  );
+}
