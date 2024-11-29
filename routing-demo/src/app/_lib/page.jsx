9edad04370039a6339084b18f7647d5321a89aee
ashokneupane100 <ashokabrother@gmail.com>
2024-11-29 19:48:18 +0545

    Creating Private folders in Next.js

---
+++ routing-demo/src/app/_lib/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function PrivateRoute() {
+  return (
+    <div>
+        <h1>You can not view this 
+            on the browser.
+        </h1>
+
+
+    </div>
+  )
+}
---
+++ routing-demo/src/app/dashboard/lineChart.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function page() {
+  return (
+    <div>Thsi is the valid page for the nextjs.</div>
+  )
+}
